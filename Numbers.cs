2cf129d967db22b52fb2a4264c5c5b56dfca6836
ziiaa <azzizahnur01@amikom.ac.id>
2022-04-07 21:26:29 +0700

    first commit

---
+++ Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int i, j;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            i = number1 < number3 ? number1 : (int)number3;
+            j = number1 < number2 ? number1 : (int)number3;
+            return number1 < number2 ? i : j;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            i = number1 > number3 ? number1 : (int)number3;
+            j = number1 > number2 ? number1 : (int)number3;
+            return number1 > number2 ? i : j;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 4, 2));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3, 1));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 2, 4));
+
+            Console.ReadKey();
+        }
+    }
+}
